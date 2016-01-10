706a0e09fe7344b6a5bbeb8d4f7ff2a3ffc5da31
Philip Huysmans <philip.huysmans@gmail.com>
2016-01-10 11:56:45 +0000

    config changes

--- app.js
+++ app.js
@@ -8,10 +8,20 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    //res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
+
 var app = express();
 
 if(app.get('env') === "development") {
-  require('dotenv').load();
+  require('dotenv').load({path: '/home/pi/apps/weightWatcherApi/.env'});
   console.log("Loading dotEnv.");
 }
 
@@ -34,6 +44,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(allowCrossDomain);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
